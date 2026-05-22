f0e6c4d8076ed2c42291f1ddffaa366a14513f2d
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-22 13:16:31 +0530

    feat: add OrderDetails entity with ManyToOne relationships to CustomerOrder and Product

---
+++ src/main/java/com/devapp/pos/entity/OrderDetails.java
@@ -0,0 +1,34 @@
+package com.devapp.pos.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_details")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "order_details_id")
+    private UUID orderDetailId;
+
+    @Column(name = "unit_price")
+    private double unitPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private CustomerOrder customerOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "quentity")
+    private Integer quantity;
+}
