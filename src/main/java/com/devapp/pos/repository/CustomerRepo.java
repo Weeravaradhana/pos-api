c3f6a9ed1f43f1653aa4aa8b77f9269126fbbffe
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-22 20:55:37 +0530

    feat(customer): add customer search and pagination queries to repository

--- src/main/java/com/devapp/pos/repository/CustomerRepo.java
+++ src/main/java/com/devapp/pos/repository/CustomerRepo.java
@@ -1,10 +1,19 @@
 package com.devapp.pos.repository;
 
 import com.devapp.pos.entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import java.util.UUID;
 
 @EnableJpaRepositories
 public interface CustomerRepo extends JpaRepository<Customer, UUID> {
+
+    @Query(value = "SELECT * FROM customer WHERE name LIKE ?1 OR address LIKE ?1", nativeQuery = true)
+    public Page<Customer> countAllCustomers(String SearchText, Pageable pageable);
+
+    @Query(value = "SELECT COUNT(*) FROM customer WHERE name LIKE ?1 OR address LIKE ?1", nativeQuery = true)
+    public long countAllCustomers(String SearchText);
 }
