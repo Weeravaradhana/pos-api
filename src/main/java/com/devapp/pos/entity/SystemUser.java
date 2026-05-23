b6a9bb3d1063eeda926e6f2d24c1099c992abcdd
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-23 13:55:21 +0530

    feat(auth): add SystemUser entity with role-based access support

---
+++ src/main/java/com/devapp/pos/entity/SystemUser.java
@@ -0,0 +1,31 @@
+package com.devapp.pos.entity;
+
+import com.devapp.pos.enums.ROLE_TYPES;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "system_user")
+@Builder
+public class SystemUser{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false, length = 100)
+    private String fullName;
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ROLE_TYPES role;
+    @Column(nullable = false)
+    private boolean isActive = true;
+}
