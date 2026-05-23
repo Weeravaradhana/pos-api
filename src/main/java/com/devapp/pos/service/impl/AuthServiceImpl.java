2d691d1fbf2fbdb1caa97ef0a3763fb41e3a3e07
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-23 21:47:07 +0530

    feat: integrate AuthenticationManager and JWT token generation for user login authentication

--- src/main/java/com/devapp/pos/service/impl/AuthServiceImpl.java
+++ src/main/java/com/devapp/pos/service/impl/AuthServiceImpl.java
@@ -8,7 +8,10 @@ import com.devapp.pos.enums.ROLE_TYPES;
 import com.devapp.pos.exception.DuplicateEntryException;
 import com.devapp.pos.repository.SystemUserRepo;
 import com.devapp.pos.service.AuthService;
+import com.devapp.pos.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,9 @@ public class AuthServiceImpl implements AuthService {
 
     private final SystemUserRepo systemUserRepo;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
 
     @Override
     public void register(RegisterRequestDto dto) {
@@ -37,6 +43,25 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public AuthResponseDTO login(LoginRequestDto dto) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getEmail(),
+                        dto.getPassword()
+                )
+        );
+
+       SystemUser systemUser = systemUserRepo.findSystemUserByEmail(dto.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found")
+                        );
+
+       String token = jwtUtil.generateAccessToken(systemUser);
+
+       return AuthResponseDTO.builder()
+               .token(token)
+               .email(systemUser.getEmail())
+               .fullName(systemUser.getFullName())
+               .role(String.valueOf(systemUser.getRole()))
+               .tokenType("Bearer")
+               .build();
     }
 }
