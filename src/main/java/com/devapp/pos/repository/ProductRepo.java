4a12c5f9e8f6c246d46d71f7eea4ee2cc93b3bef
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-23 11:11:18 +0530

    feat(product): add product search and pagination queries to repository

--- src/main/java/com/devapp/pos/repository/ProductRepo.java
+++ src/main/java/com/devapp/pos/repository/ProductRepo.java
@@ -1,10 +1,19 @@
 package com.devapp.pos.repository;
 
 import com.devapp.pos.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import java.util.UUID;
 
 @EnableJpaRepositories
 public interface ProductRepo extends JpaRepository<Product, UUID> {
+
+    @Query(value = "SELECT * FROM product WHERE description LIKE ?1", nativeQuery = true)
+    public Page<Product> findAllProducts(String  SearchText, Pageable pageable);
+
+    @Query(value = "SELECT COUNT(*) FROM product WHERE description LIKE ?1", nativeQuery = true)
+    public long countAllProducts(String SearchText);
 }
