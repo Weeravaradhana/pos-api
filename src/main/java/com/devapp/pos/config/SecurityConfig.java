a8a5e08c8c6b4697dd8271364dcbcc963128811d
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-23 21:05:39 +0530

    feat: implement JWT authentication and role-based authorization with Spring Security

--- src/main/java/com/devapp/pos/config/SecurityConfig.java
+++ src/main/java/com/devapp/pos/config/SecurityConfig.java
@@ -2,24 +2,29 @@ package com.devapp.pos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private static final String[] paths={"/auth/**"};
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,JwtAuthFilter jwtAuthFilter) throws Exception{
         http
                 .cors(cors->cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
@@ -29,8 +34,10 @@ public class SecurityConfig {
                         auth -> auth
                                 .requestMatchers(paths).permitAll()
                                 .anyRequest().authenticated()
-                );
-
+                )
+        .addFilterBefore(
+                jwtAuthFilter, UsernamePasswordAuthenticationFilter.class
+        );
 
         return http.build();
     }
