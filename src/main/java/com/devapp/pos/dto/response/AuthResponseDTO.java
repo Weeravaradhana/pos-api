830d1656362435e4795486f67b787a56d1e3e130
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-23 13:51:38 +0530

    feat(auth): add AuthResponseDTO for authentication responses

---
+++ src/main/java/com/devapp/pos/dto/response/AuthResponseDTO.java
@@ -0,0 +1,16 @@
+package com.devapp.pos.dto.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AuthResponseDTO {
+    private String token;
+    private String tokenType;
+    private String email;
+    private String fullName;
+    private String role;
+}
